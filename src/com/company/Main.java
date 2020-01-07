3c45d42387289b8e724a6f664bb09541e775f279
soniachander7294 <sonia3081jaisal@gmail.com>
2020-01-07 15:06:38 -0500

    my second project

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a=3;
+	int b=5;
+	int sum=a+b;
+        System.out.println("sum is:" +sum);
+
+    }
+}
